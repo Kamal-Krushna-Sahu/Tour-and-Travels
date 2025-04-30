d53350c969b59c668ceaecf98b10cf6679a7e978
Kamal-Krushna-Sahu <sahukamalkrushna369@gmail.com>
2025-04-30 20:51:13 +0530

    updated footer

--- frontend/src/components/ui/Footer.jsx
+++ frontend/src/components/ui/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-full p-2'>
+    <div className='w-full p-2 shadow-lg md:text-xl'>
       <h2 className='text-center text-blue-400'>Copyright © 2025. All rights reserved.</h2>
     </div>
   )
