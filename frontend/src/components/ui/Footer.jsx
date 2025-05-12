0c1f2389dcfabc6f26ba2be1b88c74f85855f9c7
Kamal-Krushna-Sahu <sahukamalkrushna369@gmail.com>
2025-05-12 21:05:16 +0530

    worked on footer

--- frontend/src/components/ui/Footer.jsx
+++ frontend/src/components/ui/Footer.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-full p-2 shadow-lg md:text-xl'>
-      <h2 className='text-center text-blue-400'>Copyright © 2025. All rights reserved.</h2>
+    <div className="w-full p-2 md:text-xl bg-white">
+      <h2 className="text-center text-blue-400">
+        Copyright © 2025. All rights reserved.
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
